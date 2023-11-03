9a8dc38abad5cad75668dca9ce754156314e1272
Mohamed <89197318+MW7000@users.noreply.github.com>
2023-11-03 11:21:07 +0200

    Update SRVM_private.h

--- HAL/SRVM/SRVM_private.h
+++ HAL/SRVM/SRVM_private.h
@@ -10,7 +10,7 @@
 #define  SRVM_PRIVATE_H
 
 
-// for controlling Servo motor, actual DC values should be between 2.4% and 12% instead of the theoretical values [5%,10%] for full range [0,180]
+// for controlling Servo motor, actual DC values should be between 2.5% and 12% instead of the theoretical values [5%,10%] for full range [0,180]
 // DC is multiplied here by 10 to enable floating-point inputs for the TIM1_voidFGeneratePWM(uint8 copy_uint8Freq , uint16 *copy_puint16DutyCycle)
 #define    SRVM_MAX_DC         120
 #define    SRVM_MIN_DC         25
